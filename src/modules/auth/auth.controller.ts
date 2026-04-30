9ce1d77bc41b949644f896b7d78e27852f529cbd
reinsaemani <reinsaemani@gmail.com>
2026-04-30 15:26:32 +0700

    v1.0.33

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -18,7 +18,8 @@ export const cookieOptions = () => {
 export const register = async (req: Request, res: Response) => {
   try {
     const user = await authService.register(req.body);
-    res.json({ ...user, message: "User registered successfully" });
+    const { password, ...safeUser } = user;
+    res.json({ ...safeUser, message: "User registered successfully" });
   } catch (err: any) {
     res.status(400).json({
   success: false,
@@ -36,17 +37,13 @@ export const login = async (req: Request, res: Response) => {
 
     // ACCESS TOKEN (boleh di response)
     res.cookie("accessToken", result.accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax",
+      ...cookieOptions(),
       maxAge: 15 * 60 * 1000, // 15 menit
     });
 
     // REFRESH TOKEN (WAJIB HTTP ONLY)
     res.cookie("refreshToken", result.refreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax",
+      ...cookieOptions(),
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari
     });
 
@@ -85,7 +82,11 @@ export const logout = async (req: Request, res: Response) => {
     const refreshToken = req.cookies.refreshToken;
 
     if (refreshToken) {
-      const decoded = jwt.decode(refreshToken) as any;
+      const decoded = jwt.verify(
+          refreshToken,
+          process.env.JWT_REFRESH_SECRET!
+        ) as any;
+
 
       if (decoded?.id) {
         await prisma.user.update({
@@ -123,6 +124,10 @@ export const resetPassword = async (req: any, res: Response) => {
 export const refreshToken = async (req: Request, res: Response) => {
   const token = req.cookies.refreshToken;
 
+  if (!process.env.JWT_SECRET || !process.env.JWT_REFRESH_SECRET) {
+      throw new Error("JWT env missing");
+    }
+
   if (!token) {
     return res.status(401).json({ message: "No refresh token" });
   }
@@ -141,11 +146,16 @@ export const refreshToken = async (req: Request, res: Response) => {
       return res.status(403).json({ message: "Invalid refresh token" });
     }
 
+    
+    const refreshSecret = process.env.JWT_REFRESH_SECRET;
+    if (!refreshSecret) throw new Error("Missing JWT_REFRESH_SECRET");
+
     const newAccessToken = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET as string,
       { expiresIn: "15m" }
     );
+    
 
     res.cookie("accessToken", newAccessToken, {
       ...cookieOptions(),
